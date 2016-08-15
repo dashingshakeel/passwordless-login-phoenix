3c8bdd558351b33daf749a79321f152873f0bce9
dashingshakeel <shkeelrauf@gmail.com>
2016-08-15 12:33:18 +0500

    configured for heroku.com

--- README.md
+++ README.md
@@ -7,11 +7,8 @@ To start your Phoenix app:
   * Install Node.js dependencies with `npm install`
   * Start Phoenix endpoint with `mix phoenix.server`
 
-Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
-
-Ready to run in production? Please [check our deployment guides](http://www.phoenixframework.org/docs/deployment).
-
-## Learn more
+you can visit  for online watching
+http://stormy-fjord-92501.herokuapp.com/
 
   * Official website: http://www.phoenixframework.org/
   * Guides: http://phoenixframework.org/docs/overview
--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :passwordless_login_app, PasswordlessLoginApp.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
-
+  url: [scheme: "https", host: "stormy-fjord-92501.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :passwordless_login_app, PasswordlessLoginApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
