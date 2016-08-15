13b820b6eb5037682831f4aaee526a92cb452083
dashingshakeel <shkeelrauf@gmail.com>
2016-08-15 12:13:28 +0500

    mailgun correction

--- lib/passwordless_login_app/mailer.ex
+++ lib/passwordless_login_app/mailer.ex
@@ -6,7 +6,7 @@ defmodule PasswordlessLoginApp.Mailer do
 
   def send_login_token(%User{email: email, access_token: token}) do
     send_email to: email,
-    from: "dashingshak@outlook.com",
+    from: "dashing.mydomain.com",
     subject: "Your token",
     text: "Access your account #{token_url(token)}"
   end
